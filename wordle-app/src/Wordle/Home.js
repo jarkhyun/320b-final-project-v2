dda67e3438a0dce1d4e745e5a50a2f883f1fed02
Ryan Ku <rjhk@uw.edu>
2022-06-23 22:43:48 -0700

    added secrets and firebaseui legacy-peer-deps

--- wordle-app/src/Wordle/Home.js
+++ wordle-app/src/Wordle/Home.js
@@ -1,38 +1,23 @@
 import React from 'react';
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase/compat/app';
 import '../db';
 
 export default function Home() {
-
-    const provider = new GoogleAuthProvider();
-    const auth = getAuth();
-    
-    const signIn = () => {
-        signInWithPopup(auth, provider)
-        .then((result) => {
-            // This gives you a Google Access Token. You can use it to access the Google API.
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            // The signed-in user info.
-            const user = result.user;
-            // ...
-        }).catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.customData.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            // ...
-        });
-    }
-
-    
+    const uiConfig = {
+        signInFlow: 'popup', 
+        signInSuccessUrl: '/',
+        signInOptions: [
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        ],
+    };
 
     return (
         <div>
-            <input id={`google-sign-in`} className={`signin`} type="button" value={`Sign in`} onClick={() => {signIn()}}/>
+            <StyledFirebaseAuth
+                uiConfig={uiConfig}
+                firebaseAuth={firebase.auth()}
+            />
         </div>
     );
 };
