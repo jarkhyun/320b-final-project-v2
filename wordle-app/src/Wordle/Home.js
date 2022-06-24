9e5069713127990a87a50dc8ddda128a041dfef9
Ryan Ku <rjhk@uw.edu>
2022-06-24 00:31:15 -0700

    removed StyledFirebaseAuth added firebaseui

--- wordle-app/src/Wordle/Home.js
+++ wordle-app/src/Wordle/Home.js
@@ -1,23 +1,46 @@
 import React from 'react';
-import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from 'firebase/compat/app';
+import * as firebaseui from 'firebaseui'
+import 'firebaseui/dist/firebaseui.css'
 import '../db';
 
 export default function Home() {
-    const uiConfig = {
-        signInFlow: 'popup', 
-        signInSuccessUrl: '/',
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+    var uiConfig = {
+        callbacks: {
+          signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+            // User successfully signed in.
+            // Return type determines whether we continue the redirect automatically
+            // or whether we leave that to developer to handle.
+            return true;
+          },
+          uiShown: function() {
+            // The widget is rendered.
+            // Hide the loader.
+            document.getElementById('loader').style.display = 'none';
+          }
+        },
+        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+        signInFlow: 'popup',
+        signInSuccessUrl: '</>',
         signInOptions: [
-            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        ],
-    };
+          // Leave the lines as is for the providers you want to offer your users.
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        //   firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        //   firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        //   firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        //   firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        //   firebase.auth.PhoneAuthProvider.PROVIDER_ID
+        ]
+      };
+      
+    ui.start('#firebaseui-auth-container', uiConfig);
 
     return (
         <div>
-            <StyledFirebaseAuth
-                uiConfig={uiConfig}
-                firebaseAuth={firebase.auth()}
-            />
+            <div id="firebaseui-auth-container"></div>
+            <div id="loader">Loading...</div>
         </div>
     );
 };
