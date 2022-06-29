0c62ef07676a2ffa9f346bccaa80636ba61c5f80
Ryan Ku <rjhk@uw.edu>
2022-06-28 21:51:37 -0700

    additional css

--- wordle-app/src/Wordle/Nav.js
+++ wordle-app/src/Wordle/Nav.js
@@ -1,7 +1,7 @@
 // import React, { useEffect, useState } from 'react';
 import React from 'react';
 // import { Link, useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 // import firebase from 'firebase/compat/app';
 // import { getAuth, onAuthStateChanged } from "firebase/auth";
 
